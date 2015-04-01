a973660988a123e0de4f7379dfe5be7eec1438db
Degenerated1123 <a.taulien@live.de>
2015-04-01 23:03:36 +0200

    Added missing files

---
+++ D3D11Engine/zCPolyStrip.h
@@ -0,0 +1,45 @@
+#pragma once
+#include "pch.h"
+#include "HookedFunctions.h"
+#include "zCPolygon.h"
+#include "Engine.h"
+#include "GothicAPI.h"
+
+class zCPolyStrip
+{
+public:
+	zCPolygon* GetPolyList()
+	{
+		return *(zCPolygon**)THISPTR_OFFSET(GothicMemoryLocations::zCPolyStrip::Offset_PolyList);
+	}
+
+	int GetNumPolys()
+	{
+		return *(int*)THISPTR_OFFSET(GothicMemoryLocations::zCPolyStrip::Offset_NumPolys);
+	}
+
+	/** Generates a vertex-buffer from the poly-list */
+	void GenerateVertexBuffer(std::vector<ExVertexStruct>& vx)
+	{
+		int num = GetNumPolys();
+		zCPolygon* polyArray = GetPolyList();
+
+		for(int i=0;i<num;i++)
+		{
+			zCPolygon* poly = &polyArray[i];
+
+			std::vector<ExVertexStruct> polyFan;
+			for(int v=0;v<poly->GetNumPolyVertices();v++)
+			{
+				ExVertexStruct t;
+				t.Position = poly->getVertices()[v]->Position;
+				t.TexCoord = poly->getFeatures()[v]->texCoord;
+				t.Normal = poly->getFeatures()[v]->normal;
+				t.Color = poly->getFeatures()[v]->lightStatic;
+			}
+
+			// Make a triangle list
+			WorldConverter::TriangleFanToList(&polyFan[0], polyFan.size(), &vx);
+		}
+	}
+};
