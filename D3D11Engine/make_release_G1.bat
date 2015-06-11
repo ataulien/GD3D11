edf2b1c94f6adc3a49fc3f62e231aca747cc07d9
Degenerated1123 <a.taulien@live.de>
2015-06-11 23:12:30 +0200

    Reduced world glossiness after rain has stopped Using default normalmap now for wet surfaces which don't have their own Fixed chapter-screens not being visible Updated G1-Version to include rain

---
+++ D3D11Engine/make_release_G1.bat
@@ -0,0 +1,12 @@
+echo OFF
+set p=%CD%
+
+echo Creating ZIP for G1...
+
+del ..\PreviewBuilds\GD3D11_PreviewReleaseG1.zip
+cd /d %G1_SYSTEM_PATH%
+CALL 7z a -tzip -mm=LZMA -mx=9 %p%\GD3D11_PreviewReleaseG1.zip @%p%\make_release_listfile.txt
+echo %p%
+cd /d %p%
+ 
+move GD3D11_PreviewReleaseG1.zip ..\PreviewBuilds\GD3D11_PreviewReleaseG1.zip
