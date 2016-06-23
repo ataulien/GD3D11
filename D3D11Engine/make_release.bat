52f5788dbc6403311acd191ffefbaaf451938e47
Jan <Katharsas@users.noreply.github.com>
2016-06-24 01:30:35 +0200

    Better render setting defaults for AMD cards, release update
    - disabled tesselation per default
    - set DrawWorldMesh to 3 ( if <=2 -> worldmesh not visible)

--- D3D11Engine/make_release.bat
+++ D3D11Engine/make_release.bat
@@ -1,20 +1,12 @@
 echo OFF
-echo Creating ZIP for G2...
 set p=%CD%
 
+echo Creating ZIP for G2...
+
 del ..\PreviewBuilds\GD3D11_PreviewReleaseG2.zip
-cd %G2_SYSTEM_PATH%
+cd /d %G2_SYSTEM_PATH%
 CALL 7z a -tzip %p%\GD3D11_PreviewReleaseG2.zip @%p%\make_release_listfile.txt
 echo %p%
-cd %p%
-
-echo Creating ZIP for G1...
-
-REM del ..\PreviewBuilds\GD3D11_PreviewReleaseG1.zip
-REM cd /d %G1_SYSTEM_PATH%
-REM CALL 7z a -tzip -mm=LZMA -mx=9 %p%\GD3D11_PreviewReleaseG1.zip @%p%\make_release_listfile.txt
-REM echo %p%
-REM cd /d %p%
+cd /d %p%
  
 move GD3D11_PreviewReleaseG2.zip ..\PreviewBuilds\GD3D11_PreviewReleaseG2.zip
-REM move GD3D11_PreviewReleaseG1.zip ..\PreviewBuilds\GD3D11_PreviewReleaseG1.zip
