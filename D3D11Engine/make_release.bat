c52a984b2f6e5abd48b148fbde8cd0022bb908b1
Jan <Katharsas@users.noreply.github.com>
2017-03-27 14:52:47 +0200

    Fix make_release bats (for paths with spaces)

--- D3D11Engine/make_release.bat
+++ D3D11Engine/make_release.bat
@@ -5,7 +5,7 @@ echo Creating ZIP for G2...
 
 del ..\PreviewBuilds\GD3D11_PreviewReleaseG2.zip
 cd /d %G2_SYSTEM_PATH%
-CALL 7z a -tzip %p%\GD3D11_PreviewReleaseG2.zip @%p%\make_release_listfile.txt
+CALL 7z a -tzip "%p%\GD3D11_PreviewReleaseG2.zip" @"%p%\make_release_listfile.txt"
 echo %p%
 cd /d %p%
  
